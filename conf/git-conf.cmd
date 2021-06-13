159ddec68f72a3c1bd473f140a2e61c4b9b4c301
c332030 <c332030@gmail.com>
2021-06-13 14:28:11 +0800

    feat: 加速

--- conf/git-conf.cmd
+++ conf/git-conf.cmd
@@ -6,4 +6,7 @@ git config --global core.autocrlf input
 
 git config --global core.safecrlf true
 
+git config --global http.proxy socks5://127.0.0.1:10808
+git config --global https.proxy socks5://127.0.0.1:10808
+
 pause>nul
