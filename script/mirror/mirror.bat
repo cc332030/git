e027fe9b1710bd2a8e582c349a515fad4da5dd2c
c332030 <c332030@gmail.com>
2021-08-02 20:32:45 +0800

    feat: not delete

--- script/mirror/mirror.bat
+++ script/mirror/mirror.bat
@@ -17,16 +17,12 @@ cd %work_path%
 
 git pull origin main
 
-rmdir /s /q .\.git >nul 2>&1
-
-echo.
-git init
-
 git add -A
 
 echo.
 git commit -m init
 
+git branch -M main
 git remote add origin %git_remote%
 
 echo.
